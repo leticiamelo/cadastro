6f2977d080278c7044263c5c44b07bdf2554f39d
Leticia Melo <erica.lety@hotmail.com>
2019-12-31 14:33:32 +0000

    csrf

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -6,7 +6,7 @@
 			<h5 class="card-title"> Cadastro de Produtos</h5>
 	
 			
-			<table class="table table-ordered table-hover">
+			<table class="table table-ordered table-hover" id="tabelaProdutos">
 				<thead>
 					<tr>
 						<th>Código</th>
@@ -83,8 +83,14 @@
 @endsection
 
 @section('javascript')
+<script type="text/javascript">
 
-	<script type="text/javascript">
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-TOKEN':"{{ csrf_token() }}"
+		}
+	});
+
 		function novoProduto(){
 			$('#id').val('');
 			$('#nomeProduto').val('');
@@ -103,10 +109,37 @@
 			});
 		}
 
+		function montarLinha(p){
+			var linha = "<tr>" +
+					"<td>" + p.id + "</td>" +
+					"<td>" + p.nome + "</td>" +
+					"<td>" + p.estoque + "</td>" +
+					"<td>" + p.preco + "</td>" +
+					"<td>" + p.categoria_id + "</td>" +
+					"<td>" +
+					  '<button class="btn btn-sm btn-primary"> Editar </button>'	+
+					  '<button class="btn btn-sm btn-danger"> Apagar </button>'	+
+					"</td>" +
+					"</tr>";
+				return linha;	
+
+		}
+
+		function carregarProdutos(){
+			$.getJSON('/api/produtos', function(produtos){
+
+				for(i=0;i<produtos.length;i++){
+					linha=montarLinha(produtos[i]);
+					$('#tabelaProdutos>tbody').append(linha);
+				}
+			})
+		}
+
 		$(function(){
 			carregarCategorias();
+			carregarProdutos();
 		});
 
-	</script>
+</script>
 	
 @endsection
